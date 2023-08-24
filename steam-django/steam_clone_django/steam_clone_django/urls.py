ce4722e0e7387eb4e353f4d19a050841d7ca58e3
Mazen Yasser <mazenyasser225@gmail.com>
2023-08-24 15:40:14 +0300

    Added static to urls.py

--- steam-django/steam_clone_django/steam_clone_django/urls.py
+++ steam-django/steam_clone_django/steam_clone_django/urls.py
@@ -28,4 +28,4 @@ urlpatterns = [
     path('paypal/', include('paypal.standard.ipn.urls')),
     
     
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root= settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
